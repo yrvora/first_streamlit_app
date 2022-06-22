5c8cb8b75ba64215daf8517b87a3df33af5b83c5
yrvora <107978155+yrvora@users.noreply.github.com>
2022-06-22 17:24:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-# import snowflake.connector
+import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Mom New Healthy Dinner')
